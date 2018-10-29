f1d46288aa97771fdcdb3d3c6c8cf31dc7575020
Mohammed Faizuddin <faiz@avantari.org>
2018-10-29 20:12:58 +0530

    initial commit flow-app-component-toggle

---
+++ src/index.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import Switch from 'react-switch';
+import AppComponent from 'flow-app-component';
+
+import './css/theme/default.css';
+
+class ToggleComponent extends AppComponent {
+  constructor() {
+    super();
+    const newState = {
+      properties: [
+        {
+          categoryName: 'General',
+          categoryDescription: 'Basic settings for the toggle',
+          properties: [],
+        },
+        {
+          categoryName: 'Events',
+          categoryDescription: 'Events for the toggle button',
+          properties: [
+            {
+              id: 'event',
+              name: 'Events',
+              type: 'graph',
+              options: {},
+              data: null,
+            },
+          ],
+        },
+      ],
+      iconUrl: '/assets/images/toggle-component.png',
+      name: 'Toggle',
+      type: 'ui-component',
+      componentType: 'toggle',
+      category: 'Inputs',
+      parent: null,
+      showOnComponentsPanel: true,
+      isValuable: true,
+      allowsChildren: false,
+    };
+
+    this.state = Object.assign(this.state, newState); // merge two states together, and dont lose any parent state properties.
+  }
+
+  renderContent() {
+    return (
+      <div className="toggle-container">
+        <label htmlFor="toggle">
+          <Switch
+            onColor="#86d3ff"
+            onHandleColor="#2693e6"
+            handleDiameter={19}
+            height={15}
+            width={30}
+            uncheckedIcon={false}
+            checkedIcon={false}
+            boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
+            activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
+            id="toggle"
+            onChange={this.toggleSwitch}
+            checked={this.state.toggleState}
+            className="toggle-input"
+          />
+          <span>Toggle</span>
+        </label>
+      </div>
+    );
+  }
+}
+
+export default ToggleComponent;
