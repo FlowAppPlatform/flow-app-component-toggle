a852d6ba392e8edee77eaee21b3d4029a4872316
Mohammed Faizuddin <faiz@avantari.org>
2018-10-30 17:47:59 +0530

    changes build process to use nwb

--- src/index.js
+++ src/index.js
@@ -5,6 +5,18 @@ import AppComponent from 'flow-app-component';
 import './css/theme/default.css';
 
 class ToggleComponent extends AppComponent {
+  static properties = {
+    iconUrl: '/assets/images/toggle-component.png',
+    name: 'Toggle',
+    type: 'ui-component',
+    componentType: 'toggle',
+    category: 'Inputs',
+    parent: null,
+    showOnComponentsPanel: true,
+    isValuable: true,
+    allowsChildren: false
+  };
+
   constructor() {
     super();
     const newState = {
@@ -28,15 +40,8 @@ class ToggleComponent extends AppComponent {
           ],
         },
       ],
-      iconUrl: '/assets/images/toggle-component.png',
-      name: 'Toggle',
-      type: 'ui-component',
-      componentType: 'toggle',
-      category: 'Inputs',
-      parent: null,
-      showOnComponentsPanel: true,
-      isValuable: true,
-      allowsChildren: false,
+
+      ...ToggleComponent.properties
     };
 
     this.state = Object.assign(this.state, newState); // merge two states together, and dont lose any parent state properties.
